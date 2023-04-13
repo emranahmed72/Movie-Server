e14ac6806130dfd01a6a1d1df2d67d46b601d929
imran hossain <94099080+emranahmed72@users.noreply.github.com>
2023-04-13 14:28:32 +0600

    Add files via upload

---
+++ movies react fronted/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
+
+  const getMovies = async () =>{
+    
+    try
+    {
+
+      const response = await api.get("/api/m/movies");
+
+      setMovies(response.data);
+
+
+    } 
+    catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/m/movies${movieId}`);
+
+        setMovie(response.data);
+        console.log(response.data);
+
+        setReviews(response.data.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  },[])
+  console.log(movies);
+
+  return (
+    <div className="App">
+      
+      <Header/>
+    
+      <Routes> 
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            
+          </Route>
+      </Routes> 
+      
+
+    </div>
+  );
+}
+
+export default App;
